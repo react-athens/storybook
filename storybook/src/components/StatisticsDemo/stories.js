8d5a18fe7d5704215260322e9a992ecb96f1f33b
Alexandre Xypas <a.xypas@signalocean.com>
2017-12-13 11:15:24 +0200

    Prepare demo

---
+++ storybook/src/components/StatisticsDemo/stories.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import Colors from '../Color/colors.js'
+import Statistics from './index';
+
+const stories = storiesOf('Statistics Demo', module)
+
+stories.add('Default', () => (
+  <div style={{width: 220}}>
+    <Statistics difference={100} label='label' total={120}/>
+  </div>
+))
+
+stories.add('Negative values', () => (
+  <div style={{width: 220}}>
+    <Statistics difference={-100} label='label' total={120}/>
+  </div>
+))
+
+stories.add('Without difference', () => (
+  <div style={{width: 220}}>
+    <Statistics label='label' total={44120}/>
+  </div>
+))
